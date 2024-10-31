518bb91052e5919decab2efaf297d16ad9fd8289
Erik Young <young.erik9@gmail.com>
2024-10-31 16:09:11 -0700

    fix money pipe

--- src/app/shared/pipes/money.pipe.ts
+++ src/app/shared/pipes/money.pipe.ts
@@ -7,20 +7,10 @@ export class MoneyPipe implements PipeTransform {
   constructor() {}
 
   transform(value: string | number): string {
-    const number = Number(value);
-    const isNegative = number < 0;
-    const over1000 = Math.abs(number) >= 1000;
-    let numberStr = Math.abs(number).toFixed(2);
-
-    if (over1000) {
-      numberStr = `${numberStr.slice(0, 1)},${numberStr.slice(1)}`;
-    }
-
-    numberStr = `${isNegative ? '-' : ''}$${numberStr}`;
-
-    if (numberStr.endsWith('.00')) {
-      return numberStr.slice(0, -3);
-    }
-    return numberStr;
+    const num = Number(value);
+    return num.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
   }
 }
