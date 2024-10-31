df026eb5f3899ba1ed3ef3bdd33969eb872856c8
Erik Young <young.erik9@gmail.com>
2024-10-31 08:36:32 -0700

    dependency fix

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,32 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AutoScrollComponent } from './components/auto-scroll/auto-scroll.component';
-import { Loop2Component } from './components/loop-2/loop-2.component';
-import { LoopComponent } from './components/loop/loop.component';
-import { ScrollChildComponent } from './components/scroll-child/scroll-child.component';
-import { ScrollContainerComponent } from './components/scroll-container/scroll-container.component';
 import { AutoScrollDirectiveDirective } from './directives/auto-scroll-directive.directive';
 import { MoneyPipe } from './pipes/money.pipe';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [
-    MoneyPipe,
-    ScrollContainerComponent,
-    ScrollChildComponent,
-    LoopComponent,
-    Loop2Component,
-    AutoScrollComponent,
-    AutoScrollDirectiveDirective,
-  ],
-  exports: [
-    MoneyPipe,
-    ScrollContainerComponent,
-    ScrollChildComponent,
-    LoopComponent,
-    Loop2Component,
-    AutoScrollComponent,
-    AutoScrollDirectiveDirective,
-  ],
+  declarations: [MoneyPipe, AutoScrollComponent, AutoScrollDirectiveDirective],
+  exports: [MoneyPipe, AutoScrollComponent, AutoScrollDirectiveDirective],
 })
 export class SharedModule {}
